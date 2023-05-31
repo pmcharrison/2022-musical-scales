18647dad14be0eb0b141e150a5bf57801e6747da
Peter Harrison <pmch2@cam.ac.uk>
2023-05-31 10:49:11 +0100

    Remove old scripts folder

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-. scripts/params.sh
-
-echo "Building the experiment's Docker image and tagging it $EXPERIMENT_IMAGE..."
-
-DOCKER_BUILDKIT=1 \
-  docker build \
-  --build-arg PSYNET_EDITABLE="${PSYNET_EDITABLE-}" \
-  -t "${EXPERIMENT_IMAGE}" \
-  .
--- scripts/debug-bash.sh
+++
@@ -1,8 +0,0 @@
-set -euo pipefail
-
-. scripts/params.sh
-
-docker exec \
-  -it \
-  dallinger \
-  /bin/bash
